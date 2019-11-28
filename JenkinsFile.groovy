09767c6b85a75cfb661967a3dbcfeafc1df85815
pridebj88 <pridebj88@gmail.com>
2019-11-28 19:51:36 +0900

    젠킨스파일
    Change-Id: Ic7b90d029e53af73e7e7a233e13bed37ea6fd673

---
+++ JenkinsFile.groovy
@@ -0,0 +1,23 @@
+pipeline {
+    agent any
+    stages {
+        stage('Example') {
+            steps {
+                echo 'Hello World3'
+            }
+        }
+    }
+    //마지막 어떻게 할껀지
+    post {
+        always {
+            echo 'build done!!!!!'
+        }
+        failure {
+            echo 'build Fail!!!!!'
+            //mail to: team@gmail.com, subject: 'Pipeline fail email'
+        }
+    }
+}
+pipeline {
+    /* insert Declarative Pipeline here */
+}
