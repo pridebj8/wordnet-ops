5206638d105c193cda55b9c71bc000917f61f7ee
최혜인 <hyein0058@gmail.com>
2019-12-03 22:33:29 +0900

    커밋5

--- src/main/java/com/wordnet/ops/controller/TestController.java
+++ src/main/java/com/wordnet/ops/controller/TestController.java
@@ -1,14 +1,12 @@
 package com.wordnet.ops.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TestController {
 
-    @RequestMapping(value="/hi")
-    @ResponseBody
+    @GetMapping(value="/hi")
     public String test(){
         return "test";
     }
